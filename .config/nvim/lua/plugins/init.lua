1ef640f90c19074e62d2a7e0c9a1f21d4d6cbc6b
Zayn Chen <zaynchen@qq.com>
2022-05-05 11:35:05 +0800

    macos 22.5.4 nvim add telescope+gitsigns

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,15 +3,40 @@ local plug_path = vim.fn.expand("<sfile>:p:h").."/plugins"
 
 vim.call("plug#begin", plug_path)
 
+Plug "nvim-lua/plenary.nvim"
+-- Web apis
+Plug "mattn/webapi-vim"
+
+
 -- Color Scheme
 Plug "RRethy/nvim-base16"
 
 -- Status Line
 Plug "nvim-lualine/lualine.nvim"
 
--- -- LSP
-Plug "neovim/nvim-lspconfig"
-Plug "nvim-lua/lsp-status.nvim"
+-- Display
+-- Plug "kien/rainbow_parentheses.vim"
+-- Plug "luochen1990/rainbow"
+--
+Plug "p00f/nvim-ts-rainbow"
+Plug "lukas-reineke/indent-blankline.nvim"
+
+-- Syntax Highlight
+-- Plug "sheerun/vim-polyglot"
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
+
+-- File Explorer
+Plug "kyazdani42/nvim-web-devicons"
+Plug("kyazdani42/nvim-tree.lua", { on = "NvimTreeToggle" })
+
+-- Finder
+Plug "nvim-telescope/telescope.nvim"
+
+-- Search
+-- Plug "eugen0329/vim-esearch"
+Plug "kevinhwang91/nvim-hlslens"
+-- TODO
+-- Plug "mg979/vim/visual-multi"
 
 -- Completion
 Plug "hrsh7th/cmp-nvim-lsp"
@@ -29,59 +54,38 @@ Plug "quangnguyen30192/cmp-nvim-ultisnips"
 -- Comment
 Plug "tpope/vim-commentary"
 
--- File Explorer
-Plug "kyazdani42/nvim-web-devicons"
-Plug("kyazdani42/nvim-tree.lua", { on = "NvimTreeToggle" })
+-- Editorconfig
+Plug "editorconfig/editorconfig-vim"
+
+-- Tool
+Plug "wakatime/vim-wakatime"
+Plug "folke/which-key.nvim"
+-- this if for im switch, lick fcitx switch, but has bug on macos
+-- Plug "rlue/vim-barbaric"
+
+-- Git
+Plug "lewis6991/gitsigns.nvim"
+
+-- LSP
+Plug "neovim/nvim-lspconfig"
+Plug "nvim-lua/lsp-status.nvim"
 
 -- Rust
 Plug "simrat39/rust-tools.nvim"
 Plug "saecki/crates.nvim"
 
-Plug "nvim-lua/plenary.nvim"
--- Plug "mfussenegger/nvim-dap"
-
 -- Julia
 Plug "JuliaEditorSupport/julia-vim"
 
--- Search
--- Plug "eugen0329/vim-esearch"
-Plug "kevinhwang91/nvim-hlslens"
-
--- Web apis
-Plug "mattn/webapi-vim"
-
-
--- TODO
--- Plug "mg979/vim/visual-multi"
-
--- Display
--- Plug "kien/rainbow_parentheses.vim"
--- Plug "luochen1990/rainbow"
---
-Plug "p00f/nvim-ts-rainbow"
-Plug "lukas-reineke/indent-blankline.nvim"
-
--- Syntax Highlight
--- Plug "sheerun/vim-polyglot"
-Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
-
--- Editorconfig
-Plug "editorconfig/editorconfig-vim"
-
 -- Debug
 -- TODO
+-- Plug "mfussenegger/nvim-dap"
 Plug "sakhnik/nvim-gdb"
 
 -- TODO
 -- Plug "iamcco/markdown-preview.nvim"
 
 Plug("lervag/vimtex", { ["for"] = "tex" })
--- this if for im switch, lick fcitx switch, but has bug on macos
--- Plug "rlue/vim-barbaric"
-
--- Tool
-Plug "wakatime/vim-wakatime"
-Plug "folke/which-key.nvim"
 
 vim.call("plug#end")
 
@@ -107,10 +111,12 @@ require("nvim-web-devicons").setup({})
 require("plugins/lualine")
 require("plugins/nvim-tree")
 require("plugins/nvim-cmp")
+require("plugins/treesitter")
 require("plugins/lspconfig")
+require("plugins/telescope")
+require("plugins/gitsigns")
 require("plugins/crates")
 require("plugins/vimtex")
-require("plugins/treesitter")
 
 -- barbaric
 -- g.barbaric_ime = "macos"
---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+local nmap = require('mappings').nmap
+
+local builtin = require("telescope.builtin")
+
+nmap("<leader>ff", builtin.find_files)
+nmap("<leader>fg", builtin.live_grep)
+nmap("<leader>fb", builtin.buffers)
+nmap("<leader>fh", builtin.help_tags)
+-- nmap("<leader>fj", builtin.jumplist)
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        -- ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
