93bdff3b344aabc1772655f2ea5136257237a3bf
Zayn Chen <zaynchen@qq.com>
2022-05-08 14:20:36 +0800

    macos 22.5.8 telescope utils deprecated temp fix & alacritty im keymap

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,8 @@
+-- The nvim-treesitter.ts_utils.get_node_text is deprecated
+-- TODO
+local ts_utils = require("nvim-treesitter.ts_utils")
+ts_utils.get_node_text = vim.treesitter.query.get_node_text
+
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "c", "lua", "python", "rust", "ruby", "yaml", "json", "markdown" },
   sync_install = false,
