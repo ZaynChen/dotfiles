6d6c57dd6a622fdea619fa22c6eee7721b22c231
Zayn Chen <zaynchen@qq.com>
2022-07-25 11:16:40 +0800

    +u config_pacman.sh v2

--- .bin/config_pacman.sh
+++ .bin/config_pacman.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-sudo pacman -Syu
 
 # Colorize output
 PATTERN="#Color"
 REPLACEMENT="Color"
-sudo sed -i "/$PATTERN/c\
-$REPLACEMENT" /etc/pacman.conf
+sudo sed -i "s/$PATTERN/$REPLACEMENT/g" /etc/pacman.conf
 
 # Add archlinuxcn repo
 if [ -z "$(cat /etc/pacman.conf | grep -i archlinuxcn)" ]
 then
   echo '[archlinuxcn]
 Server = https://repo.archlinuxcn.org/$arch' | sudo tee -a /etc/pacman.conf
- 
 fi
 
 # Setup mirrors
