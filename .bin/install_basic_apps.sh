952ba6eedaa86919ad9918054e650de5c719fa6a
Zayn Chen <zaynchen@qq.com>
2022-07-27 14:45:24 +0800

    +u scripts & fctix5 theme

--- .bin/install_basic_apps.sh
+++ .bin/install_basic_apps.sh
@@ -1,39 +1,40 @@
 sudo pacman -S paru --noconfirm --needed
 
-sudo paru -S archlinuxcn-keyring --noconfirm --needed
+paru -S archlinuxcn-keyring --noconfirm --needed
 
-sudo paru -S picom --noconfirm --needed
+paru -S picom --noconfirm --needed
 
-sudo paru -S lightdm lightdm-webkit-theme-litarvan --noconfirm --needed
+paru -S lightdm lightdm-webkit-theme-litarvan --noconfirm --needed
 PATTERN="antergos"
 REPLACEMENT="litarvan"
 sudo sed -i "s/$PATTERN/$REPLACEMENT/g" /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo systemctl enable lightdm.service
 
-sudo paru -S betterlockscreen --noconfirm --needed
+paru -S betterlockscreen --noconfirm --needed
 betterlockscreen -u /usr/share/backgrounds/archlinux
 
-sudo paru -S awesome --noconfirm --needed
-sudo paru -S lain-git --noconfirm --needed
+paru -S awesome --noconfirm --needed
+paru -S lain-git --noconfirm --needed
 
-sudo paru -S xdg-utils --noconfirm --needed
-sudo paru -S alsa-utils --noconfirm --needed
-sudo paru -S volumeicon --noconfirm --needed
-sudo paru -S pcmanfm --noconfirm --needed
-sudo paru -S fcitx5 fcitx5-chinese-addons fcitx5-nord --noconfirm --needed
+paru -S xdg-utils --noconfirm --needed
+paru -S alsa-utils --noconfirm --needed
+paru -S volumeicon --noconfirm --needed
+paru -S pcmanfm --noconfirm --needed
+paru -S fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-material-color --noconfirm --needed
 
-sudo paru -S peazip --noconfirm --needed
-sudo paru -S fd --noconfirm --needed
-sudo paru -S ranger --noconfirm --needed
-sudo paru -S exa --noconfirm --needed
-sudo paru -S ripgrep --noconfirm --needed
+paru -S peazip --noconfirm --needed
+paru -S fd --noconfirm --needed
+paru -S ranger --noconfirm --needed
+paru -S exa --noconfirm --needed
+paru -S ripgrep --noconfirm --needed
 
-sudo paru -S htop --noconfirm --needed
-sudo paru -S neofetch --noconfirm --needed
+paru -S htop --noconfirm --needed
+paru -S neofetch --noconfirm --needed
 
-sudo paru -S alacritty --noconfirm --needed
-sudo paru -S tmux --noconfirm --needed
+paru -S alacritty --noconfirm --needed
+paru -S neovim --noconfirm --needed
+paru -S tmux --noconfirm --needed
 
-sudo paru -S firefox-developer-edition firefox-developer-edition-i18n-zh-cn --noconfirm --needed
+paru -S firefox-developer-edition firefox-developer-edition-i18n-zh-cn --noconfirm --needed
 
-sudo paru -S clash-for-windows-chinese --noconfirm --needed
+paru -S clash-for-windows-chinese --noconfirm --needed
