42df1b19ba8a16dd8338bc4c25f5b83c9f56f247
Zayn Chen <zaynchen@qq.com>
2022-08-04 22:00:16 +0800

    +using xdg base directories specification

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s .config $XDG_CONFIG_HOME
 ln -s .bin $HOME/.bin
 ln -s .pam_enviroment $HOME/.pam_environment
 ln -s .config/zsh/.zshenv $HOME/.zshenv
+ln -s .xprofile $HOME/.xprofile
 
 # oh-my-zsh
 # compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
