9a408a6d915ab8e22a900afc56d7eb3c2789ea24
Zayn Chen <zaynchen@qq.com>
2022-08-23 20:33:54 +0800

    update install_lsp.sh

--- .bin/install_lsp.sh
+++ .bin/install_lsp.sh
@@ -1,3 +1,7 @@
 sudo paru -S pyright --noconfirm --needed
+sudo paru -S python-black --noconfirm --needed
 sudo paru -S lua-language-server --noconfirm --needed
-
+sudo paru -S bash-language-server --noconfirm --needed
+sudo paru -S rust-analyzer --noconfirm --needed
+sudo paru -S yaml-language-server --noconfirm --needed
+sudo paru -S cmake-language-server --noconfirm --needed
