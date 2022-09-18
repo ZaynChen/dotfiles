76ae499f75d7507e6c51c654f665019ada7fe5b3
Zayn Chen <zaynchen@qq.com>
2022-09-18 15:34:46 +0800

    Update pacman config

--- .bin/config_pacman.sh
+++ .bin/config_pacman.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # Colorize output
-PATTERN="#Color"
-REPLACEMENT="Color"
-sudo sed -i "s/$PATTERN/$REPLACEMENT/g" /etc/pacman.conf
+PATTERN="^#Color$"
+FIND="^#"
+REPLACE=""
+sudo sed -i "/$PATTERN/s/$FIND/$REPLACE/g" /etc/pacman.conf
+
+# Enable multilib
+PATTERN="^#\[multilib\]/,/^$"
+FIND="^#"
+REPLACE=""
+sudo sed -i "/$PATTERN/s/$FIND/$REPLACE/g" /etc/pacman.conf
 
 # Add archlinuxcn repo
 if [ -z "$(grep -i "archlinuxcn" /etc/pacman.conf)" ]; then
-  echo '[archlinuxcn]
+  echo '
+[archlinuxcn]
 Server = https://repo.archlinuxcn.org/$arch' | sudo tee -a /etc/pacman.conf
 fi
+
+sudo pacman -S archlinuxcn-keyring --noconfirm --needed
+sudo pacman -Sy
 
 # Setup mirrors
 sudo pacman -S reflector --noconfirm --needed
