8458e8849552f068ac016f8c62dfdb160ce64dbb
Zayn Chen <zaynchen@qq.com>
2022-09-18 15:41:40 +0800

    Repo: update installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Directies and files should not exist before running this script
-ln -s .bin $HOME/.bin
-ln -s .config $XDG_CONFIG_HOME
+ln -s ./.bin $HOME/.bin
+ln -s ./.config $HOME/.config
 
 # oh-my-zsh
 # compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
