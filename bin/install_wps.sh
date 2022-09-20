1eb659c461054177c10e7f5c7483c5c8fce73f05
Zayn Chen <zaynchen@qq.com>
2022-09-20 16:22:37 +0800

    wps: Localization

--- bin/install_wps.sh
+++ bin/install_wps.sh
@@ -1,4 +1,5 @@
 paru -S wps-office-cn --noconfirm --needed
+paru -S wps-office-mui-zh-cn --noconfirm --needed
 paru -S wps-office-fonts --noconfirm --needed
 paru -S ttf-wps-fonts --noconfirm --needed
 paru -S wps-office-all-dicts-win-languages --noconfirm --needed
