e8183df0b65181602d9d16f3d668a99c6b759469
Zayn Chen <zaynchen@qq.com>
2022-10-07 16:15:32 +0800

    update(conda): update miniconda3 installation script

--- bin/install_miniconda3.sh
+++ bin/install_miniconda3.sh
@@ -1,7 +1,7 @@
 paru -S miniconda3 --noconfirm --needed
 
-if [ -z "$(grep -i miniconda3 ~/.zshrc)" ] ; then
-  echo "[ -f /opt/miniconda3/etc/profile.d/conda.sh ] && source /opt/miniconda3/etc/profile.d/conda.sh" >> ~/.zshrc
-fi
+# if [ -z "$(grep -i miniconda3 ~/.conig/zsh/.zshrc)" ] ; then
+#   echo "[ -f /opt/miniconda3/etc/profile.d/conda.sh ] && source /opt/miniconda3/etc/profile.d/conda.sh" >> ~/.config/zsh/.zshrc
+# fi
 
 ln -s /opt/miniconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
