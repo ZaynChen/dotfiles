091f21c8f25c271c67a0d5f665c82800c5be8cc6
Zayn Chen <zaynchen@qq.com>
2022-11-29 16:13:42 +0800

    neovim: update lsp buf.format

--- config/nvim/lua/keymap/lsp.lua
+++ config/nvim/lua/keymap/lsp.lua
@@ -9,7 +9,7 @@ M.on_attach = function(bufnr)
     { "gd", function() lsp.buf.definition() end, "[LSP]Definition" },
     { "K", function() lsp.buf.hover() end, "[LSP]Hover" },
     { "<leader>la", function() lsp.buf.code_action() end, "[LSP]Code action" },
-    { "<leader>lf", function() lsp.buf.formatting() end, "[LSP]Formatting" },
+    { "<leader>lf", function() lsp.buf.format { async = true } end, "[LSP]Formatting" },
     { "<leader>lr", function() lsp.buf.rename() end, "[LSP]Rename" },
   }
   for _, mapping in ipairs(mappings) do
