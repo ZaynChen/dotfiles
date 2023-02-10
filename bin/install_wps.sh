6d153667f059b1bfcfa216742ea3632feb336315
Zayn Chen <zaynchen@qq.com>
2023-02-10 16:12:16 +0800

    update wps install scripts

--- bin/install_wps.sh
+++ bin/install_wps.sh
@@ -2,4 +2,7 @@ paru -S wps-office-cn --noconfirm --needed
 paru -S wps-office-mui-zh-cn --noconfirm --needed
 paru -S wps-office-fonts --noconfirm --needed
 paru -S ttf-wps-fonts --noconfirm --needed
+paru -S wps-office-mime-cn --noconfirm --needed
 paru -S wps-office-all-dicts-win-languages --noconfirm --needed
+
+paru -S libtiff5 --noconfirm --needed
