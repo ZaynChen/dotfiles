2d8e3d448ae562954ed72fbc6fe8600c362b7a29
Zayn Chen <zaynchen@qq.com>
2023-04-05 20:08:53 +0800

    config awesome keybindings

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -281,9 +281,9 @@ awful.keyboard.append_global_keybindings({
     { description = "open a terminal", group = "launcher" }),
   awful.key({ modkey, }, "b", function() awful.spawn(browser) end,
     { description = "open a browser", group = "launcher" }),
-  awful.key({ modkey }, "space", function() awful.screen.focused().mypromptbox:run() end,
+  awful.key({ modkey, }, "space", function() awful.screen.focused().mypromptbox:run() end,
     { description = "run prompt", group = "launcher" }),
-  awful.key({ modkey }, "p", function() menubar.show() end,
+  awful.key({ modkey, }, "p", function() menubar.show() end,
     { description = "show the menubar", group = "launcher" }),
 })
 
@@ -319,11 +319,11 @@ awful.keyboard.append_global_keybindings({
       end
     end,
     { description = "go back", group = "client" }),
-  awful.key({ modkey, "Control" }, "j", function() awful.screen.focus_relative(1) end,
+  awful.key({ modkey, controlkey }, "j", function() awful.screen.focus_relative(1) end,
     { description = "focus the next screen", group = "screen" }),
-  awful.key({ modkey, "Control" }, "k", function() awful.screen.focus_relative(-1) end,
+  awful.key({ modkey, controlkey }, "k", function() awful.screen.focus_relative(-1) end,
     { description = "focus the previous screen", group = "screen" }),
-  awful.key({ modkey, "Control" }, "n",
+  awful.key({ modkey, controlkey }, "n",
     function()
       local c = awful.client.restore()
       -- Focus restored client
@@ -336,9 +336,9 @@ awful.keyboard.append_global_keybindings({
 
 -- Layout related keybindings
 awful.keyboard.append_global_keybindings({
-  awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
+  awful.key({ modkey, shiftkey }, "j", function() awful.client.swap.byidx(1) end,
     { description = "swap with next client by index", group = "client" }),
-  awful.key({ modkey, "Shift" }, "k", function() awful.client.swap.byidx(-1) end,
+  awful.key({ modkey, shiftkey }, "k", function() awful.client.swap.byidx(-1) end,
     { description = "swap with previous client by index", group = "client" }),
   awful.key({ modkey, }, "u", awful.client.urgent.jumpto,
     { description = "jump to urgent client", group = "client" }),
@@ -346,17 +346,17 @@ awful.keyboard.append_global_keybindings({
     { description = "increase master width factor", group = "layout" }),
   awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.05) end,
     { description = "decrease master width factor", group = "layout" }),
-  awful.key({ modkey, "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
+  awful.key({ modkey, shiftkey }, "h", function() awful.tag.incnmaster(1, nil, true) end,
     { description = "increase the number of master clients", group = "layout" }),
-  awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
+  awful.key({ modkey, shiftkey }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
     { description = "decrease the number of master clients", group = "layout" }),
-  awful.key({ modkey, "Control" }, "h", function() awful.tag.incncol(1, nil, true) end,
+  awful.key({ modkey, controlkey }, "h", function() awful.tag.incncol(1, nil, true) end,
     { description = "increase the number of columns", group = "layout" }),
-  awful.key({ modkey, "Control" }, "l", function() awful.tag.incncol(-1, nil, true) end,
+  awful.key({ modkey, controlkey }, "l", function() awful.tag.incncol(-1, nil, true) end,
     { description = "decrease the number of columns", group = "layout" }),
   -- awful.key({ modkey, }, "space", function() awful.layout.inc(1) end,
   --   { description = "select next", group = "layout" }),
-  awful.key({ modkey, "Shift" }, "space", function() awful.layout.inc(-1) end,
+  awful.key({ modkey, shiftkey }, "space", function() awful.layout.inc(-1) end,
     { description = "select previous", group = "layout" }),
 })
 
@@ -376,7 +376,7 @@ awful.keyboard.append_global_keybindings({
     end,
   },
   awful.key {
-    modifiers   = { modkey, "Control" },
+    modifiers   = { modkey, controlkey },
     keygroup    = "numrow",
     description = "toggle tag",
     group       = "tag",
@@ -389,7 +389,7 @@ awful.keyboard.append_global_keybindings({
     end,
   },
   awful.key {
-    modifiers   = { modkey, "Shift" },
+    modifiers   = { modkey, shiftkey },
     keygroup    = "numrow",
     description = "move focused client to tag",
     group       = "tag",
@@ -403,7 +403,7 @@ awful.keyboard.append_global_keybindings({
     end,
   },
   awful.key {
-    modifiers   = { modkey, "Control", "Shift" },
+    modifiers   = { modkey, controlkey, shiftkey },
     keygroup    = "numrow",
     description = "toggle focused client on tag",
     group       = "tag",
@@ -475,13 +475,13 @@ client.connect_signal("request::default_keybindings", function()
         c:raise()
       end,
       { description = "(un)maximize", group = "client" }),
-    awful.key({ modkey, "Control" }, "m",
+    awful.key({ modkey, controlkey }, "m",
       function(c)
         c.maximized_vertical = not c.maximized_vertical
         c:raise()
       end,
       { description = "(un)maximize vertically", group = "client" }),
-    awful.key({ modkey, "Shift" }, "m",
+    awful.key({ modkey, shiftkey }, "m",
       function(c)
         c.maximized_horizontal = not c.maximized_horizontal
         c:raise()
@@ -558,6 +558,11 @@ ruled.client.connect_signal("request::rules", function()
   }
 
   ruled.client.append_rule {
+    rule_any   = { class = { "QQ" } },
+    properties = { screen = 1, tag = "5" }
+  }
+
+  ruled.client.append_rule {
     rule_any   = { class = { "netease-cloud-music" } },
     properties = { screen = 1, tag = "6" }
   }
@@ -584,10 +589,10 @@ end)
 client.connect_signal("request::titlebars", function(c)
   -- buttons for the titlebar
   local buttons = {
-    awful.button({}, 1, function()
+    awful.button({}, mouse_left, function()
       c:activate { context = "titlebar", action = "mouse_move" }
     end),
-    awful.button({}, 3, function()
+    awful.button({}, mouse_right, function()
       c:activate { context = "titlebar", action = "mouse_resize" }
     end),
   }
