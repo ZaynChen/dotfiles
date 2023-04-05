d3ea53a334964ce3492eea119da2292d30c1f501
Zayn Chen <zaynchen@qq.com>
2023-04-05 21:46:38 +0800

    change fonts

--- bin/install_fonts.sh
+++ bin/install_fonts.sh
@@ -1,6 +1,6 @@
 sudo paru -S adobe-source-code-pro-fonts --noconfirm --needed
 sudo paru -S adobe-source-han-sans-cn-fonts --noconfirm --needed
 sudo paru -S wqy-zenhei --noconfirm --needed
-sudo paru -S nerd-fonts-fira --noconfirm --needed
-sudo paru -S nerd-fonts-fira-code --noconfirm --needed
+sudo paru -S otf-firamono-nerd --noconfirm --needed
+sudo paru -S ttf-firacode-nerd --noconfirm --needed
 
