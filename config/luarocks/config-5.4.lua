542bda67d936a7c2d6d4b0d05c8b8b8a02a12bd9
Zayn Chen <zaynchen@qq.com>
2023-04-05 20:28:33 +0800

    setup luarocks config

--- config/luarocks/config-5.4.lua
+++ config/luarocks/config-5.4.lua
@@ -1,11 +1,11 @@
 -- LuaRocks configuration
 
 rocks_trees = {
-  { name = "user", root = home .. "/.config/luarocks" };
-  { name = "system", root = "/usr" };
+  { name = "user",   root = home .. "/.local/share/luarocks" },
+  { name = "system", root = "/usr" },
 }
 lua_interpreter = "lua5.4";
 variables = {
-  LUA_DIR = "/usr";
-  LUA_BINDIR = "/usr/bin";
+  LUA_DIR = "/usr",
+  LUA_BINDIR = "/usr/bin",
 }
