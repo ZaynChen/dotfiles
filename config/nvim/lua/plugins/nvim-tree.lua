a60e09eb905e1ae13dfd6b544bb2aaebf4958902
Zayn Chen <zaynchen@qq.com>
2023-04-11 22:17:43 +0800

    remove deprecated opts

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -74,9 +74,6 @@ local DEFAULT_OPTS = {
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
   sort_by = "name",
   root_dirs = {},
   prefer_startup_root = false,
@@ -187,7 +184,6 @@ local DEFAULT_OPTS = {
     update_root = false,
     ignore_list = {},
   },
-  ignore_ft_on_setup = {},
   system_open = {
     cmd = "",
     args = {},
