d990882507bb80eca8f72c1021aed85b55afe38a
Zayn Chen <zaynchen@qq.com>
2023-04-26 14:12:47 +0800

    rust-analyzer is in rustup now

--- bin/install_lsp.sh
+++ bin/install_lsp.sh
@@ -2,6 +2,6 @@ paru -S pyright --noconfirm --needed
 paru -S python-black --noconfirm --needed
 paru -S lua-language-server --noconfirm --needed
 paru -S bash-language-server --noconfirm --needed
-paru -S rust-analyzer --noconfirm --needed
+rustup component add rust-analyzer
 paru -S yaml-language-server --noconfirm --needed
 paru -S cmake-language-server --noconfirm --needed
