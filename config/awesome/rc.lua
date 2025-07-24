386c76044dbca4ab744f3a6b5bebd245b646cd21
Zayn Chen <zaynchen@qq.com>
2025-07-24 13:35:31 +0800

    awesomewm sets mihomo-party default screen

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -577,7 +577,7 @@ ruled.client.connect_signal("request::rules", function()
   }
 
   ruled.client.append_rule {
-    rule_any   = { class = { "Clash for Windows", "Mihomo Party" } },
+    rule_any   = { class = { "Clash for Windows", "mihomo-party" } },
     properties = { screen = 1, tag = "7" }
   }
 
