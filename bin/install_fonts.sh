e4f99313d4b17408b9651243c0b3994d86b7482a
Zayn Chen <zaynchen@qq.com>
2025-11-06 05:53:24 +0800

    fix(fonts): install_fonts.sh add missing italic fonts (JetbrainsMono)

--- bin/install_fonts.sh
+++ bin/install_fonts.sh
@@ -3,8 +3,10 @@ paru -S adobe-source-han-sans-cn-fonts --noconfirm --needed
 paru -S wqy-zenhei --noconfirm --needed
 paru -S otf-firamono-nerd --noconfirm --needed
 paru -S ttf-firacode-nerd --noconfirm --needed
+paru -S ttf-jetbrains-mono-nerd --noconfirm --needed
 paru -S ttf-liberation --noconfirm --needed
 paru -S ttf-juliamono --noconfirm --needed
 
 paru -S lib32-fontconfig --noconfirm --needed
+paru -S font-manager --noconfirm --needed
 
