6ed0354657a0bf6f02f7b72d53d9b7ae894ec38d
Zayn Chen <zaynchen@qq.com>
2025-11-07 00:00:58 +0800

    feat(neovim): add ts-context-commentstring plugin to enpower Comment.nvim on commenting files in which contains multiple language code all in one (like Vue).

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,74 +1,59 @@
 return {
   "numToStr/Comment.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring"
+  },
   opts = {
-    ---Add a space b/w comment and the line
-    ---@type boolean|fun():boolean
+    --- Controls space between the comment and the line (default: 'true')
     padding = true,
 
-    ---Whether the cursor should stay at its position
-    ---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
-    ---@type boolean
+    --- Whether the cursor should stay at its position
+    --- Only works in NORMAL mode mappings (default: 'true')
     sticky = true,
 
-    ---Lines to be ignored while comment/uncomment.
-    ---Could be a regex string or a function that returns a regex string.
-    ---Example: Use '^$' to ignore empty lines
-    ---@type string|fun():string
+    --- Lua pattern used to ignore lines during (un)comment (default: 'nil')
     ignore = nil,
 
-    ---LHS of toggle mappings in NORMAL mode
-    ---@type table
-    toggler = {
-      ---Line-comment toggle keymap
-      line = 'gcc',
-      ---Block-comment toggle keymap
-      block = 'gbc',
-    },
-
-    ---LHS of operator-pending mappings in NORMAL mode
-    ---LHS of mapping in VISUAL mode
-    ---@type table
-    opleader = {
-      ---Line-comment keymap
-      line = 'gc',
-      ---Block-comment keymap
-      block = 'gb',
-    },
-
-    ---LHS of extra mappings
-    ---@type table
-    extra = {
-      ---Add comment on the line above
-      above = 'gcO',
-      ---Add comment on the line below
-      below = 'gco',
-      ---Add comment at the end of line
-      eol = 'gcA',
-    },
-
-    ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
-    ---NOTE: If `mappings = false` then the plugin won't create any mappings
-    ---@type boolean|table
+    --- Enable keybindings
+    --- NOTE: If given `false` then the plugin won't create any mappings
     mappings = {
-      ---Operator-pending mapping
-      ---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
-      ---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+      ---Operator-pending mapping; `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
       basic = true,
-      ---Extra mapping
-      ---Includes `gco`, `gcO`, `gcA`
+      ---Extra mapping; `gco`, `gcO`, `gcA`
       extra = true,
-      ---Extended mapping
-      ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
-      extended = false,
     },
 
-    ---Pre-hook, called before commenting the line
-    ---@type fun(ctx: CommentCtx):string
-    pre_hook = nil,
+    --- LHS of toggle mappings in NORMAL mode
+    toggler = {
+      --- Line-comment toggle keymap
+      line = "gcc",
+      --- Block-comment toggle keymap
+      block = "gbc",
+    },
 
-    ---Post-hook, called after commenting is done
-    ---@type fun(ctx: CommentCtx)
+    --- LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+      --- Line-comment keymap
+      line = "gc",
+      --- Block-comment keymap
+      block = "gb",
+    },
+
+    --- LHS of extra mappings
+    extra = {
+      --- Add comment on the line above
+      above = "gcO",
+      --- Add comment on the line below
+      below = "gco",
+      --- Add comment at the end of line
+      eol = "gcA",
+    },
+
+    --- Function to call before (un)comment
+    pre_hook = function() return vim.bo.commentstring end,
+
+    --- Function to call after (un)comment
     post_hook = nil,
   },
 }
