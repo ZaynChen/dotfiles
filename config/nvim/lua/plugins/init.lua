803475c2538a212f6f5c2ff6bde7b8d8fc535486
Zayn Chen <zaynchen@qq.com>
2026-04-08 18:09:46 +0800

    chore(nvim): update nvim plugins

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-  { "norcalli/nvim-colorizer.lua", event = "BufReadPost", opts = { "*" } },
+  { "catgoose/nvim-colorizer.lua", event = "BufReadPre",  opts = {} },
   { "andymass/vim-matchup",        event = "BufReadPost" },
   { "kevinhwang91/nvim-hlslens",   event = "BufReadPost", opts = {} },
   { "wakatime/vim-wakatime",       event = "VeryLazy" },
